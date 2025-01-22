eed872013cbad634fef1a4816f24864dcd0ea387
kvandana451 <kvandana451@gmail.com>
2025-01-22 22:20:49 +0530

    explored class binding ,by implementing toggle feature

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -5,8 +5,12 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'my-app';
+  isActive = true;
+  toggle() {
+    this.isActive = !this.isActive;
+  }
 }
